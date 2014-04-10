2c54de1b03c3512f669aa7ac2b038686d801233e
jlyonsmith <john@lyon-smith.org>
2014-04-09 19:47:54 -0700

    Fixing tests for Travis.

--- bin/travis_test.sh
+++ bin/travis_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
-NUNIT_DIR=$(find . -type d -name packages/CSharpTools\* -maxdepth 1)/tools
+NUNIT_DIR=$(find packages -type d -name NUnit.Runners\* -maxdepth 1)/tools
 mono --runtime=v4.0 $NUNIT_DIR/nunit-console.exe -noxml -nodots -labels -stoponerror $@
 exit $?
