44a99b3e65210f6c5b73f8e5c946c7efa32a91c3
jlyonsmith <john@lyon-smith.org>
2014-05-30 20:01:24 -0700

    Adding PCL version of ServiceStack library.

---
+++ ToolBelt.ServiceStack.Pcl/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyTitle("ToolBelt.ServiceStack.Pcl")]
+[assembly: AssemblyDescription("Portable Programmers ToolBelt for ServiceStack")]
+[assembly: AssemblyCompany("John Lyon-Smith")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("(c) 2014, John Lyon-Smith")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.20528.3")]
