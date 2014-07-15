6023d0751aa982bd62e4c5d9f35b607c2c418cc7
jlyonsmith <john@lyon-smith.org>
2014-07-15 15:56:21 +0200

    Adding deployment scripts

--- bin/nuget_pack.sh
+++ bin/nuget_pack.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 mono ~/lib/NuGet/NuGet.exe pack ToolBelt.nuspec
+mono ~/lib/NuGet/NuGet.exe pack ToolBelt.MongoDB.nuspec
 mono ~/lib/NuGet/NuGet.exe pack ToolBelt.ServiceStack.nuspec
 mono ~/lib/NuGet/NuGet.exe pack ToolBelt.ServiceStack.Pcl.nuspec
