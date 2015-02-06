7d8fa0304f47831791149d33bea2d4222121055c
jlyonsmith <john@lyon-smith.org>
2015-02-06 12:30:24 -0800

    Adding ByteArrayExtensions.ToHex()

---
+++ Tests/ToolBelt.Tests/ByteArrayExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ToolBelt.Tests
+{
+    [TestFixture]
+    public class ByteArrayExtensionsTests
+    {
+        [Test]
+        public void ByteArrayToHexString()
+        {
+            var bytes1 = new byte[] { 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10 };
+            var bytes2 = new byte[] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef };
+
+            Assert.AreEqual("fedcba9876543210", bytes1.ToHex());
+            Assert.AreEqual("0123456789abcdef", bytes2.ToHex());
+        }
+    }
+}
+
---
+++ ToolBelt/Collections/ByteArrayExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ToolBelt
+{
+    public static class ByteArrayExtensions
+    {
+        public static string ToHex(this byte[] p)
+        {
+            char[] c = new char[p.Length * 2];
+            byte b;
+
+            for (int y = 0, x = 0; y < p.Length; ++y, ++x)
+            {
+                b = ((byte)(p[y] >> 4));
+                c[x] = (char)(b > 9 ? b + 'a' - 10 : b + '0');
+                b = ((byte)(p[y] & 0xF));
+                c[++x] = (char)(b > 9 ? b + 'a' - 10 : b + '0');
+            }
+
+            return new string(c);
+        }
+    }
+}
+
