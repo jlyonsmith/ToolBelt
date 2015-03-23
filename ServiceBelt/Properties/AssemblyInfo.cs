063c6b86cf7e9bf763a5b110048ee21b531150bc
jlyonsmith <john@lyon-smith.org>
2015-03-22 20:49:03 -0700

    Release 3.0.20322.0

--- ServiceBelt/Properties/AssemblyInfo.cs
+++ ServiceBelt/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.20315.0")]
+[assembly: AssemblyFileVersion("3.0.20322.0")]
