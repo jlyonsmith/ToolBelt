d49ea4165bf7f95b77743577b4dc2daf1d25a5ba
jlyonsmith <john@lyon-smith.org>
2015-03-26 14:25:37 -0700

    Update TravisCI now that it has csharp support

--- bin/travis_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -x
-NUNIT_DIR=$(find packages -type d -name NUnit.Runners\* -maxdepth 1)/tools
-mono --runtime=v4.0 $NUNIT_DIR/nunit-console.exe -noxml -nodots -labels -stoponerror $@
-exit $?
